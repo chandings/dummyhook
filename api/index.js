81fa6254bf3f41c4a19269fd7697e50163dcfef9
Chandra Badan Singh <chandings@gmail.com>
2025-02-07 01:06:36 +0530

    changing the whole project

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const app = require('./server');          // Import your Express server app
-const { createServer } = require('@vercel/node');  // Import Vercel's server creation helper
-
-module.exports = createServer(app);       // Export the wrapped Express server
