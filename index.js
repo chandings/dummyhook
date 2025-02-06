e09578e320c925f9a6940d8c3dcc1a4b156c2af8
Chandra Badan Singh <chandings@gmail.com>
2025-02-07 01:29:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 let dataStore = {};
-
+const getAllCountInLI = ()=>{
+    returnValue = "";
+    for (let key in dataStore) {
+        returnValue +=`<li>key:${key}, count: ${dataStore[key]?dataStore[key].length:0}</li>` 
+    }
+    return returnValue
+}
 // GET route for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, Vercel!');
+    let htmlStr = `<h1>Dummy Hook</h1>
+    <P>This site holds messages for any post! Apis:</p>
+    <ul>
+    <li>POST /<key>: This will save the json body</li>
+    <li>GET /count, [header Data-key:<key>]:This will give the number of requests held for the given <key></li>
+    <li>GET /dump-data, [header Data-key:<key>]:This will return an array of requests held for the given key and empty the requests <key></li>
+    </ul>
+    <h3>Current Keys held</h3>
+    <ul>${getAllCountInLI()}</ul>
+    <p>Enjoy!</p>
+    `
+    res.send(htmlStr);
 });
 
 // POST route to handle dynamic text input
