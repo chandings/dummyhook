cad1afb4901286bfb896e28722e4ff606265e091
Chandra Badan Singh <chandings@gmail.com>
2025-02-07 01:15:58 +0530

    updated the code to add data to data store

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON request body
 app.use(express.json());
 
+let dataStore = {};
+
 // GET route for the root URL
 app.get('/', (req, res) => {
     res.send('Hello, Vercel!');
 });
 
 // POST route to handle dynamic text input
-app.post('/:text', (req, res) => {
-    const inputText = req.params.text;
-    res.json({
-        message: `You sent: ${inputText}`
+app.post('/:key', (req, res) => {
+    const key = req.params.key;
+    if (!dataStore[key]) dataStore[key] = [];
+    dataStore[key].push({
+        method: req.method,
+        headers: req.headers,
+        body: req.body,
+        timestamp: new Date()
     });
+    res.json({ message: `Data stored successfully for key: ${key}` });
 });
 
 app.listen(port, () => {
